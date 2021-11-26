04bbf98c238a67b7b5653661baa789c074996fac
nostrorom <49858211+Nostrorom@users.noreply.github.com>
2021-11-26 13:45:19 +0100

    adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
         target: '#svelte',
-        // adapter: adapter(),
+        adapter: adapter(),
         // env variables
         vite: {
             define: {
