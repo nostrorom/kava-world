9ec96d66fb58103758ffa8b928edbb00e2afaeb9
nostrorom <49858211+Nostrorom@users.noreply.github.com>
2021-11-26 11:28:02 +0100

    require loader gm

--- src/lib/components/GoogleMap.svelte
+++ src/lib/components/GoogleMap.svelte
@@ -5,6 +5,8 @@
 	import Button from '$lib/components/UI/Button.svelte';
 	import Spinner from '$lib/components/UI/Spinner.svelte';
 	// import { Loader } from '@googlemaps/js-api-loader';
+	import { createRequire } from 'module';
+	const require = createRequire(import.meta.url);
 	const Loader = require('@googlemaps/js-api-loader');
 
 	const dispatch = createEventDispatcher();
